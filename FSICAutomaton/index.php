87603cb3c5e487f8151370390e92a5655f6e748f
Andres, Erythrina Nicole <2160051@slu.edu.ph>
2018-07-23 16:07:12 +0800

    Updated layout

--- FSICAutomaton/index.php
+++ FSICAutomaton/index.php
@@ -2,7 +2,7 @@
     include 'includes/connection.php';
 ?>
 <!DOCTYPE HTML>
-<html>
+<html style="overflow: scroll;">
 	<head>
 		<meta charset="UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,15 +19,56 @@
 		</div>
 		<div class="id">
 			<form method="POST">
-				<div style="margin: 0 auto;text-align: center; font-size: 18px;"><label for="user">ID</label></div>
-				<input type="text" name="user" class="user" style="width: 25%;margin: 0 auto; color: black;"><br>
+				<div style="margin: 0 auto;text-align: center; font-size: 18px;"><label for="user">ID</label></div> 
+				<input type="text" name="user" class="user" required style="width: 25%;margin: 0 auto; color: black;"><br>
 				<div style="margin: 0 auto;text-align: center; font-size: 18px;"><label for="password">Password</label></div>
-				<input type="password" name="password" class="user" style="width: 25%;margin: 0 auto; color: black;"><br>
+				<input type="password" name="password" class="user" required style="width: 25%;margin: 0 auto; color: black;"><br>
 				<div style="margin: 0 auto; width: 40%;display: flex;justify-content: center;align-items: center;">
-					<input type="submit" formaction="login.php" value="Login" class="loginButton" style="width: 28%; display: inline-block;background-color: #fc4c3f; color:white;">
+					<input type="submit" value="Login" class="loginButton" style="width: 28%; display: inline-block;background-color: #fc4c3f; color:white;">
 				</div>
-				<div style="margin: 0 auto;text-align: center;"><input type='submit' class="buttonLink" value="Request for an Account" formaction="login.php"></div>
+
+				<?php
+				    if(isset($_POST['password'])){
+				    	session_start();
+				        $user = $_POST['user'];
+				        $password = $_POST['password'];
+
+				        $query = "SELECT password FROM admin WHERE id = '$user' AND password = '$password';";
+				        $query_two = "SELECT password FROM client WHERE id = '$user' AND password = '$password';";
+				        $result = mysqli_query($conn, $query);
+				        $result_two = mysqli_query($conn, $query_two);
+				        $count = mysqli_num_rows($result);
+				        $count_two = mysqli_num_rows($result_two);
+				        if($count == 1){
+				            $_SESSION['user'] = $user;
+				            header('Location: admin.php');
+				        }else if($count_two == 1){
+				        	$_SESSION['user'] = $user;
+				            header('Location: user.php');
+				        }else{
+				        	echo "<div class='alert'>
+									  <span class='closebtn'>&times;</span> 
+									  Wrong Username or Password. Please Try Again.
+								  </div>";
+				        }
+		            }
+				?>
+
+				<div style="margin: 0 auto;text-align: center;"><input type='submit' class="buttonLink" value="Request for an Account"></div>
 			</form>		
 		</div>
+	
+	<script>
+		var close = document.getElementsByClassName("closebtn");
+		var i;
+
+		for (i = 0; i < close.length; i++) {
+		    close[i].onclick = function(){
+		        var div = this.parentElement;
+		        div.style.opacity = "0";
+		        setTimeout(function(){ div.style.display = "none"; }, 600);
+		    }
+		}
+	</script>
 	</body>
 </html>
